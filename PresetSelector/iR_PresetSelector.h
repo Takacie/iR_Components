eddb7e97a024c75fb611137276953f23ee52d2cb
Takacie <mctaiga0815@gmail.com>
2021-11-07 23:02:29 +0900

    [PresetSelector]Moved the body of the implementation to a cpp file.

--- PresetSelector/iR_PresetSelector.h
+++ PresetSelector/iR_PresetSelector.h
@@ -11,49 +11,16 @@ class iR_PresetSelector : public Component
 {
 public:
   // constructor
-  iR_PresetSelector(APVTS* apvts, iR_UserProperties* userProperties) :
-    apvts(apvts),
-    user_properties(userProperties)
-  {
-    addAndMakeVisible(list_button);
-    addAndMakeVisible(menu_button);
+  iR_PresetSelector(APVTS* apvts, iR_UserProperties* userProperties);
 
-    setLookAndFeel(&laf);
-    list_button.getFileListComponent()->setLookAndFeel(&laf);
-    menu_button.getPopupMenu()->setLookAndFeel(&laf);
-    list_button.getFileListComponent()->setVisible(false);
-  }
-
-  ~iR_PresetSelector()
-  {
-    setLookAndFeel(nullptr);
-    list_button.getFileListComponent()->setLookAndFeel(nullptr);
-    menu_button.getPopupMenu()->setLookAndFeel(nullptr);
-  }
+  ~iR_PresetSelector();
 
   // setter
-  void setPosition(int x, int y, float scale)
-  {
-    x *= scale;
-    y *= scale;
-    const auto w = getWidth();       // entire width
-    const auto lb_w = w * 0.8f;      // list_button width
-    const auto mb_w = w * 0.2f;      // menu_button width
-    const auto h = 30 * scale;       // entire height
-    const auto brow_h = 150 * scale; // file_browser height
-    const auto row_h = 25 * scale;   // row height
-
-    setBounds(x, y, w, h);
-    list_button.setBounds(0, 0, lb_w, h);
-    list_button.getFileListComponent()->setBounds(x, y + h, w, brow_h);
-    list_button.getFileListComponent()->setRowHeight(row_h);
-    menu_button.setBounds(lb_w, 0, mb_w, h);
-  }
-
+  void setPosition(int x, int y, float scale);
   void setWidth(int newWidth) { setBounds(0, 0, newWidth, 0); }
 
   // getter
-  iR_FileListComponentWrapper* getFileListComponent() { return list_button.getFileListComponent(); }
+  iR_FileListComponent* getFileListComponent() { return list_button.getFileListComponent(); }
 
 private:
   // lookandfeel
--- PresetSelector/iR_PresetSelectorComponent.h
+++ PresetSelector/iR_PresetSelectorComponent.h
@@ -18,19 +18,39 @@ class iR_DirectoryList
 {
 public:
   // constructor
-  iR_DirectoryList()
-  {
-    list.setDirectory(File::getCurrentWorkingDirectory(), false, true);
-    thread.startThread();
-  }
+  iR_DirectoryList();
 
   // getter
   DirectoryContentsList* getDirectoryContentsList() { return &list; }
 
 private:
-  TimeSliceThread thread = TimeSliceThread("preset-read-thread");
+  TimeSliceThread thread = TimeSliceThread("preset_read_thread");
   WildcardFileFilter filter = WildcardFileFilter("*.irps", "", "iNVOXRecords preset file.");
   DirectoryContentsList list = DirectoryContentsList(&filter, thread);
+};
+
+//----------------------------------------------------------------------------------------------------------------------
+// iR_FileListComponentWrapper class
+//----------------------------------------------------------------------------------------------------------------------
+class iR_FileListComponent : public FileListComponent, public FileBrowserListener
+{
+public:
+  // constructor
+  iR_FileListComponent(DirectoryContentsList& directoryContentList, APVTS* apvts);
+
+  // override
+  void selectionChanged() override {}
+  void fileClicked(const File& file, const MouseEvent& e) override {}
+  void browserRootChanged(const File& newRoot) override {}
+  void fileDoubleClicked(const File& file) override;
+  void inputAttemptWhenModal() override;
+
+  // getter
+  String getPresetName() const { return preset_name; }
+
+private:
+  String preset_name = "(default)";
+  APVTS* apvts;
 };
 
 //----------------------------------------------------------------------------------------------------------------------
@@ -40,94 +60,11 @@ class iR_PresetMenuButton : public Button
 {
 public:
   // constructor
-  iR_PresetMenuButton(iR_UserProperties* user_properties, APVTS* apvts, iR_DirectoryList* dir_list) :
-    Button(""),
-    user_properties(user_properties),
-    apvts(apvts),
-    dir_list(dir_list)
-  {
-    File initial_location = File();
-    if (user_properties->getUserSettings()->getValue("preset_dir").isNotEmpty()) {
-      initial_location = File::File(user_properties->getUserSettings()->getValue("preset_dir"));
-    }
-    dir_list->getDirectoryContentsList()->setDirectory(initial_location, false, true);
-    file_chooser = std::make_unique<FileChooser>("Select the location where you want to save the presets.",
-      initial_location, "*.irps", true, false, this);
-
-    popup_menu.addItem(1, "Save preset");
-    popup_menu.addItem(2, "Change directory");
-    popup_menu.addItem(3, "Refresh");
-  }
+  iR_PresetMenuButton(iR_UserProperties* user_properties, APVTS* apvts, iR_DirectoryList* dir_list);
 
   // override
-  void paintButton(Graphics& g, bool shouldDrawButtonAsHighlighted, bool shouldDrawButtonAsDown) override
-  {
-    const auto b = getLocalBounds();
-    const auto w = getWidth();
-    const auto h = getHeight();
-    const auto x = b.getX();
-    const auto y = b.getY();
-    const auto r = h / 2;
-
-    // draw background
-    if (shouldDrawButtonAsHighlighted) g.setColour(Colour(70, 70, 75));
-    else g.setColour(Colour(50, 50, 55));
-    g.fillEllipse(x + w - h, y, h, h);
-    g.fillRect(x, y, w - r, h);
-
-    // draw signature
-    g.setColour(Colours::white);
-    const auto sign_w = w * 0.5f;
-    const auto sign_h = h * 0.05f;
-    const auto sign_x = x + w * 0.15f;
-    Rectangle<float> sign_rect(sign_x, 0, sign_w, sign_h);
-    g.fillRect(sign_rect.withY(y + h * 0.3f - sign_h / 2));
-    g.fillRect(sign_rect.withY(y + h * 0.5f - sign_h / 2));
-    g.fillRect(sign_rect.withY(y + h * 0.7f - sign_h / 2));
-  }
-
-  void mouseDown(const MouseEvent& event) override
-  {
-    Button::mouseDown(event);
-    auto click_position = event.getPosition();
-    popup_menu.showMenuAsync(PopupMenu::Options()
-      .withPreferredPopupDirection(PopupMenu::Options::PopupDirection::downwards)
-      .withTargetComponent(this)
-      .withMinimumWidth(getWidth() * 3.f)
-      .withStandardItemHeight(getHeight() * 0.8f),
-      [&](int choice) {
-        int flag = 0;
-        switch (choice)
-        {
-        case 1:
-          flag = FileBrowserComponent::FileChooserFlags::saveMode;
-          file_chooser->launchAsync(flag, [&](const FileChooser& chooser) {
-            File save_file = chooser.getResult();
-            if (save_file.getFullPathName().isNotEmpty()) {
-              std::unique_ptr<XmlElement> preset_xml = apvts->copyState().createXml();
-              preset_xml->setAttribute("PluginName", PROJECT_NAME);
-              preset_xml->writeTo(chooser.getResult());
-            }
-            });
-          break;
-        case 2:
-          flag = FileBrowserComponent::FileChooserFlags::openMode
-            | FileBrowserComponent::FileChooserFlags::canSelectDirectories;
-          file_chooser->launchAsync(flag, [&](const FileChooser& chooser) {
-            File new_dir = chooser.getResult();
-            if (new_dir.exists()) {
-              user_properties->getUserSettings()->setValue("preset_dir", new_dir.getFullPathName());
-            }
-            });
-          break;
-        case 3:
-          dir_list->getDirectoryContentsList()->refresh();
-          break;
-        default:
-          break;
-        }
-      });
-  }
+  void paintButton(Graphics& g, bool shouldDrawButtonAsHighlighted, bool shouldDrawButtonAsDown) override;
+  void mouseDown(const MouseEvent& event) override;
 
   // getter
   PopupMenu* getPopupMenu() { return &popup_menu; }
@@ -141,115 +78,29 @@ private:
 };
 
 //----------------------------------------------------------------------------------------------------------------------
-// iR_FileListComponentWrapper class
-//----------------------------------------------------------------------------------------------------------------------
-class iR_FileListComponentWrapper : public FileListComponent, public FileBrowserListener
-{
-public:
-  // constructor
-  iR_FileListComponentWrapper(DirectoryContentsList& directoryContentList, APVTS* apvts) :
-    FileListComponent(directoryContentList),
-    apvts(apvts)
-  {
-    setAlwaysOnTop(true);
-    addListener(this);
-    setFocusContainerType(FocusContainerType::focusContainer);
-    setColour(backgroundColourId, Colour(50, 50, 55));
-  }
-
-  // override
-  void selectionChanged() override {}
-  void fileClicked(const File& file, const MouseEvent& e) override {}
-  void browserRootChanged(const File& newRoot) override {}
-  void fileDoubleClicked(const File& file) override
-  {
-    if (file.getFullPathName().isNotEmpty()) {
-      setVisible(false);
-      auto preset_xml = parseXML(file);
-      if (preset_xml->hasAttribute("PluginName") && preset_xml->getStringAttribute("PluginName") == PROJECT_NAME) {
-        auto preset_tree = ValueTree::fromXml(*preset_xml);
-        apvts->replaceState(preset_tree);
-        preset_name = file.getFileNameWithoutExtension();
-      }
-      else {
-        getLookAndFeel().playAlertSound();
-      }
-    }
-  }
-  void inputAttemptWhenModal() override
-  {
-    exitModalState(0);
-    setVisible(false);
-  }
-
-  // getter
-  String getPresetName() const { return preset_name; }
-
-private:
-  String preset_name = "(default)";
-  APVTS* apvts;
-};
-
-//----------------------------------------------------------------------------------------------------------------------
 // iR_PresetListButton class
 //----------------------------------------------------------------------------------------------------------------------
 class iR_PresetListButton : public Button
 {
 public:
   // constructor
-  iR_PresetListButton(APVTS* apvts, iR_DirectoryList* dir_list) :
-    Button(""),
-    apvts(apvts),
-    dir_list(dir_list)
-  {
-    file_list_component->setVisible(false);
-  }
+  iR_PresetListButton(APVTS* apvts, iR_DirectoryList* dir_list);
 
   // override
-  void paintButton(Graphics& g, bool shouldDrawButtonAsHighlighted, bool shouldDrawButtonAsDown) override
-  {
-    const auto b = getLocalBounds();
-    const auto w = getWidth();
-    const auto h = getHeight();
-    const auto x = b.getX();
-    const auto y = b.getY();
-    const auto r = h / 2;
-
-    // background color
-    if (shouldDrawButtonAsHighlighted) g.setColour(Colour(70, 70, 75));
-    else g.setColour(Colour(50, 50, 55));
-
-    // draw background
-    g.fillEllipse(x, y, h, h);
-    g.fillRect(x + r, y, w, h);
-
-    g.setFont(Font(getHeight() * 0.6f, Font::plain));
-    g.setColour(Colours::white);
-    const Rectangle<int> text_rect(x + r / 2, y, w, h);
-    g.drawText(file_list_component->getPresetName(), text_rect, Justification::centred);
-  }
+  void paintButton(Graphics& g, bool shouldDrawButtonAsHighlighted, bool shouldDrawButtonAsDown) override;
+  void mouseDown(const MouseEvent& event) override;
 
   // getter
-  iR_FileListComponentWrapper* getFileListComponent() { return file_list_component.get(); }
+  iR_FileListComponent* getFileListComponent() { return file_list_component.get(); }
 
   // setter
-  void setPosition(Rectangle<int> rect)
-  {
-    setBounds(rect);
-  }
-
-  void mouseDown(const MouseEvent& event) override
-  {
-    Button::mouseDown(event);
-    file_list_component->updateContent();
-    file_list_component->setVisible(!file_list_component->isVisible());
-    file_list_component->enterModalState();
-  }
+  void setPosition(Rectangle<int>& rect) { setBounds(rect); }
 
 private:
   APVTS* apvts;
   iR_DirectoryList* dir_list;
-  std::unique_ptr<iR_FileListComponentWrapper> file_list_component = std::make_unique<iR_FileListComponentWrapper>(*dir_list->getDirectoryContentsList(), apvts);
+  std::unique_ptr<iR_FileListComponent> file_list_component 
+    = std::make_unique<iR_FileListComponent>(*dir_list->getDirectoryContentsList(), apvts);
 };
 
 }
